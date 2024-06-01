07921f6a22310fb53054b455809baeeafac6678d
Abhinand S <abhinandappu2005@gmail.com>
2024-06-01 15:08:33 +0530

    Add script

---
+++ Experiment 1/prgm1.sh
@@ -0,0 +1,6 @@
+#Print numbers from 1 to 100
+
+for((i=1;i<=100;i++))
+{
+    echo $i
+}
---
+++ Experiment 1/prgm2.sh
@@ -0,0 +1,10 @@
+#Finding factorial of a number
+
+echo "Enter a number :"
+read num
+fact=1
+for((i=2;i<=num;i++))
+{
+    fact=$((fact*i))
+}
+echo "Factorial : "$fact
